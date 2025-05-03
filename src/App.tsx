00d2d99aa269ea8932ab450da5be1cd312772f43
Abdulrahman Ahmed <abdulrahman.frontend@gmail.com>
2025-05-04 01:01:12 +0300

    Updated App.tsx, package files, and vite config

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import PersistTasks from "./commponents/PersistTasks"
@@ -8,13 +8,14 @@ function App() {
   return(
     <div className="container py=5">
       <nav className="mb-4 d-flex justify-between">
-        <Link to="/" className="btn btn-outline-primary mx-1">Home</Link>
+        <Link to="/home" className="btn btn-outline-primary mx-1">Home</Link>
         <Link to="about" className="btn btn-outline-secondary">About</Link>
       </nav>
       
       <PersistTasks />
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={<Home />}/>
         <Route path="/about" element={<About />}/>
       </Routes>
     </div>
